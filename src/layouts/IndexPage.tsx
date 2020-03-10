d49090e84302ba78ccf25e1a6db1694b400f262e
songning0605 <songningning0605@gmail.com>
2020-03-11 07:58:57 +0800

    add antd@4

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from '@/layouts/styles/index.scss';
 import {NavLink} from "react-router-dom";
+import {Card} from "antd";
 
 interface IProps {
 
 }
+
+const cardRender = (name:any) => <Card
+  hoverable
+  style={{ width: 240 }}
+  cover={<img style={{background: '#ddd'}} src="https://gw.alipayobjects.com/zos/rmsportal/tXlLQhLvkEelMstLyHiN.svg" />}
+>
+  <Card.Meta title={name} description="这里可以替换为一个微应用" />
+</Card>
 
 const Index: React.FunctionComponent<IProps> = (props:IProps) =>{
   return (
@@ -14,13 +23,12 @@ const Index: React.FunctionComponent<IProps> = (props:IProps) =>{
       </header>
       <section className={styles.mainWrapper}>
         <div className="card">
-          <NavLink to="/index">首页</NavLink>
+          <NavLink to="/index">
+            {cardRender('首页')}
+          </NavLink>
         </div>
         <div className="card">
-          <NavLink to="/other">other</NavLink>
-        </div>
-        <div className={styles.reactLogo}>
-          <img className={styles.an} width="160" src="https://gw.alipayobjects.com/zos/rmsportal/tXlLQhLvkEelMstLyHiN.svg" />
+          <NavLink to="/other">{cardRender('Other')}</NavLink>
         </div>
       </section>
     </div>
